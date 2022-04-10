0c74b1805ce44827cdc60e2fdd8c6c3bd3477611
chirayu <garg104@purdue.edu>
2022-04-10 13:03:49 -0400

    added Pawn move logic

--- src/game_pieces/Pawn.cpp
+++ src/game_pieces/Pawn.cpp
@@ -11,8 +11,37 @@ class Pawn : public GamePiece {
             GamePiece::player = player;
         }
 
-        virtual bool checkMoveValidity(int origin, int dest, int* board) {
-            return true;
+        virtual bool checkMoveValidity(int initial, int final, int* board) {
+            if (player == WHITE) { // white player
+                if(board[final] == nil) { // no-kill
+                    // white pawn can move up 1 always but can only move 2 up when at start
+                    if ((initial - final == 8) ||
+                        ((initial - final == 16 && initial / 8 == 6))) {
+                            return true;
+                    } 
+                } else { // kill
+                    // white pawn can move diagonal-up 1 
+                    if ((initial - final == 7) ||
+                        (initial - final == 9)) {
+                        return true;
+                    }
+                }
+            } else { // black player
+                if (board[final] == nil) { // no-kill
+                    // black pawn can move down 1 always but can only move 2 down when at start
+                    if ((final - initial == 8) ||
+                        ((final - initial == 16 && initial / 8 == 1))) {
+                        return true;
+                    } 
+                } else { // kill
+                    // black pawn can move diagonal-down 1
+                    if ((final - initial == 7) ||
+                        (final - initial == 9 )) {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
         
 
