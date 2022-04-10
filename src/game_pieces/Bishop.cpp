5321cb6571a5f7aba60e3f72319abb85d050e714
chirayu <garg104@purdue.edu>
2022-04-10 14:29:54 -0400

    added Rook and Bishop move logic

--- src/game_pieces/Bishop.cpp
+++ src/game_pieces/Bishop.cpp
@@ -11,7 +11,34 @@ class Bishop : public GamePiece {
             GamePiece::player = player;
         }
 
-        virtual bool checkMoveValidity(int orgin, int dest, int* board) {
+        virtual bool checkMoveValidity(int initial, int final, int* board) {
+            int diff = abs(initial - final);
+
+            // check along diagonals
+            int jump = 0;
+            if (diff % 9 == 0) {
+                jump = 9;
+            } else if (diff % 7 == 0) {
+                jump = 7;
+            }
+
+            // none of the 2 possible directions
+            if (jump == 0) {
+                return false;
+            }
+
+            // get jump direction
+            jump = (initial - final) > 0 ? (0 - jump) : jump;
+            cout << "Jump is " << jump << endl;
+
+            // make sure that the way to the final is clear
+            for (int i = 1; i < (diff/abs(jump)); i++) {
+                cout << "value at " << initial + (i * jump) << " is " << board[initial + (i * jump)] << endl;
+                if (board[initial + (i * jump)] != nil) { // path is blocked
+                    return false;
+                }
+            }
+            
             return true;
         }
 
--- src/game_pieces/Rook.cpp
+++ src/game_pieces/Rook.cpp
@@ -11,7 +11,34 @@ class Rook : public GamePiece {
             GamePiece::player = player;
         }
 
-        virtual bool checkMoveValidity(int orgin, int dest, int* board) {
+        virtual bool checkMoveValidity(int initial, int final, int* board) {
+            int diff = abs(initial - final);
+
+            // check along up-down and left-right
+            int jump = 0;
+            if (diff % 8 == 0) {
+                jump = 8;
+            } else if (diff / 8 == 0) {
+                jump = 1;
+            }
+
+            // none of the 4 possible directions
+            if (jump == 0) {
+                return false;
+            }
+
+            // get jump direction
+            jump = (initial - final) > 0 ? (0 - jump) : jump;
+            cout << "Jump is " << jump << endl;
+
+            // make sure that the way to the final is clear
+            for (int i = 1; i < (diff/abs(jump)); i++) {
+                cout << "value at " << initial + (i * jump) << " is " << board[initial + (i * jump)] << endl;
+                if (board[initial + (i * jump)] != nil) { // path is blocked
+                    return false;
+                }
+            }
+            
             return true;
         }
 
