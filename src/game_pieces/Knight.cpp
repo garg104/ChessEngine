863780f07762dec636be5cd1b1a06dc300c6ebae
chirayu <garg104@purdue.edu>
2022-04-10 15:31:12 -0400

    added knight move logic

--- src/game_pieces/Knight.cpp
+++ src/game_pieces/Knight.cpp
@@ -12,12 +12,12 @@ class Knight: public GamePiece {
         }
 
         virtual bool checkMoveValidity(int initial, int final, int* board) {
-            int row = initial / 8;
-            int column = initial % 8;
-            if (row ) {
-
+            if ((abs(initial - final) == 17) ||
+                (abs(initial - final) == 15) ||
+                (abs(initial - final) == 10) ||
+                (abs(initial - final) == 6)) {
+                return true;
             }
-            
             return false;
         }
 
